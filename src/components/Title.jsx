6df995d20f102ef597e1a52410c5429108968d4e
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-02-03 17:52:40 -0800

    implement title section

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,8 @@ import './Title.scss'
 const Title = () => {
   return (
     <div className="title">
-      Title
+      <span className='t-title'>Hangman</span>
+      <span className='t-help'>Type or Select a letter</span>
     </div>
   )
 }
