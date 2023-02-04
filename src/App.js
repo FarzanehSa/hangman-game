cd2cb0dd0960f46ca9a000fabfd386fdd08420eb
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-02-04 00:48:06 -0800

    input word

--- src/App.js
+++ src/App.js
@@ -10,6 +10,37 @@ import './App.scss';
 function App() {
 
   const [myWord, setMyWord] = useState("");
+  const [wordObj, setWordObj] = useState([])
+  const [myLetter, setMyLetter] = useState();
+  const [wrongAnswer, setWrongAnswer] = useState(0);
+  const [keyboard, setKeyboard] = useState([
+    {letter: "a", show: "A", check: 0},
+    {letter: "b", show: "B", check: 0},
+    {letter: "c", show: "C", check: 0},
+    {letter: "d", show: "D", check: 0},
+    {letter: "e", show: "E", check: 0},
+    {letter: "f", show: "F", check: 0},
+    {letter: "g", show: "G", check: 0},
+    {letter: "h", show: "H", check: 0},
+    {letter: "i", show: "I", check: 0},
+    {letter: "j", show: "J", check: 0},
+    {letter: "k", show: "K", check: 0},
+    {letter: "l", show: "L", check: 0},
+    {letter: "m", show: "M", check: 0},
+    {letter: "n", show: "N", check: 0},
+    {letter: "o", show: "O", check: 0},
+    {letter: "p", show: "P", check: 0},
+    {letter: "q", show: "Q", check: 0},
+    {letter: "r", show: "R", check: 0},
+    {letter: "s", show: "S", check: 0},
+    {letter: "t", show: "T", check: 0},
+    {letter: "u", show: "U", check: 0},
+    {letter: "v", show: "V", check: 0},
+    {letter: "w", show: "W", check: 0},
+    {letter: "x", show: "X", check: 0},
+    {letter: "y", show: "Y", check: 0},
+    {letter: "z", show: "Z", check: 0},
+  ]);
 
   useEffect(() => {
     // axios.get('https://api.datamuse.com/words?sp=a&max=1')
@@ -18,20 +49,84 @@ function App() {
     .then(res => {
       setMyWord(res.data[0]);
     });
+
+    document.addEventListener("keydown", detectKeyDown, true);
+
+    return () => {
+      document.removeEventListener('keydown', detectKeyDown);
+    };
+
   }, []);
 
-  console.log(myWord);
+  useEffect(() => {
+    let wArr = [];
+    for (let i = 0; i < myWord.length; i++) {
+      wArr.push({
+        letter: myWord[i],
+        found: false
+      })
+    }
+    setWordObj(wArr);
+    setWrongAnswer(0);
+    setKeyboard(keyboard.map(row => {
+      return ({...row, check: 0})
+    }))
+  }, [myWord]);
+
+  useEffect(() => {
+    if (myLetter && myWord.indexOf(myLetter) !== -1) {
+      setKeyboard(keyboard.map(row => {
+        if (myLetter === row.letter) {
+          return ({...row, check: 1});
+        } else return row;
+      }));
+      setWordObj(wordObj.map(row => {
+        if (row.letter === myLetter) {
+          return ({...row, found: true});
+        } else {
+          return row;
+        }
+      }));
+    } 
+    if (myLetter && myWord.indexOf(myLetter) === -1) {
+      setWrongAnswer(pre => pre + 1);
+      setKeyboard(keyboard.map(row => {
+        if (myLetter === row.letter) {
+          return ({...row, check: 2});
+        } else return row;
+      }));
+    }
+  }, [myLetter]);
+
+  useEffect(() => {
+    if (!wordObj.filter(row => !row.found).length) {
+      console.log("Well Done");
+    }
+  }, [wordObj]);
+
+
+  const detectKeyDown = (e) => {
+    const regexLetter = /^[a-zA-Z]{1}$/;
+    if (regexLetter.test(e.key)) {
+      setMyLetter(e.key.toLowerCase());
+    }
+  }
+
+  console.log(myWord, myLetter);
+  console.log(wordObj);
+  console.log('❌', wrongAnswer);
+  console.log('📱', keyboard);
 
   return (
     <div className="app">
       <div className='a-title'>
         <Title />
       </div>
-      <div className='a-hangman'>
+      <div className='a-hangman' wrongAnswer={wrongAnswer}>
         <Hangman />
       </div>
-      <div className='a-word' myWord={myWord}>
-        <Word />
+      <div className='a-word'>
+        <Word myWord={myWord} myLetter={myLetter} wordObj={wordObj} setMyLetter={setMyLetter} keyboard={keyboard}/>
       </div>
       <div className='a-control'>
         <Control />
--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,9 +1,60 @@
-import './Word.scss'
+import { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
 
-const Word = ({myWord}) => {
+import './Word.scss';
+
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = 1 + i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 }
+      }
+    };
+  }
+};
+
+const Word = ({myWord, myLetter, wordObj, setMyLetter, keyboard}) => {
+
+  const myWordDiv = wordObj.map((row, index) => {
+    return (
+      <div className='letter' key={index}>
+        <span className={row.found ? 'letter-v' : 'letter-h'}>
+          {row.letter}
+        </span>
+      </div>
+    )
+  });
+
+  const charButtonArr = keyboard.map((row, index) => {
+    let classN = 'char-btn';
+    if (row.check === 1) classN += ' char-btn-correct';
+    if (row.check === 2) classN += ' char-btn-wrong';
+
+    return (
+      <button 
+        className={classN}
+        key={index}
+        onClick={() => {setMyLetter(row.letter)}}
+        disabled={row.check}>{row.show}
+        <div className={row.check === 2 ? "diag-line-show" : "diag-line-hidden"}></div>
+      </button>)
+  })
+
   return (
     <div className="word">
-      word
+      <div className='char-btns'>
+        {charButtonArr}
+      </div>
+
+      <div className='showed-correct'>
+        {myWordDiv}
+      </div>
+      
     </div>
   )
 }
