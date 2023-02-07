81fdb36a85434c049fb5e7b72ab4cfb7de13a736
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-02-07 14:06:01 -0800

    refactor control

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,62 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVolumeLow, faVolumeXmark, faMusic, faBorderNone, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faVolumeLow, faMusic, faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import Slider, { SliderThumb } from '@mui/material/Slider';
+import Slider from '@mui/material/Slider';
 import { styled } from '@mui/material/styles';
 import './Control.scss';
 
 import music from '../assets/music.wav';
 import useAudioPlayer from './useAudioPlayer';
 
-
 const PrettoSlider = styled(Slider)({
   color: '#FFF',
   height: 24,
-  // '& .MuiSlider-track': {
-  //   border: 'none',
-  // },
   '& .MuiSlider-thumb': {
     height: 0.5,
     width: 0.5,
     backgroundColor: '#fff',
-    // border: '2px solid currentColor',
     border: 'none',
     '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
       boxShadow: 'inherit',
     },
-    // '&:before': {
-    //   display: 'none',
-    // },
   },
-  // '& .MuiSlider-valueLabel': {
-  //   lineHeight: 1.2,
-  //   fontSize: 12,
-  //   background: 'unset',
-  //   padding:  0,
-  //   width: 32,
-  //   height: 32,
-  //   borderRadius: '50% 50% 50% 0',
-  //   // backgroundColor: '#52af77',
-  //   transformOrigin: 'bottom left',
-  //   transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-  //   '&:before': { display: 'none' },
-  //   '&.MuiSlider-valueLabelOpen': {
-  //     transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-  //   },
-  //   // '& > *': {
-  //   //   transform: 'rotate(45deg)',
-  //   // },
-  // },
 });
 
 const Control = ({volume, setVolume, musicVolume, setMusicVolume}) => {
 
   const { playing, setPlaying } = useAudioPlayer(musicVolume);
-
   const [openHelp, setOpenHelp] = useState(false);
 
   const handleVolumeChange = (event, newValue) => {
@@ -71,25 +43,24 @@ const Control = ({volume, setVolume, musicVolume, setMusicVolume}) => {
     setOpenHelp(false);
   }
 
-
   return (
     <div className="control">
       <Modal
-          isOpen={openHelp}
-          onRequestClose={closeModal}
-          appElement={document.getElementById('root')}
-          className="a-modal"
-        >
-          <div className='help'>
-            <span>Try to get the secret word one letter at a time!</span>
-            <span>Keep in mind you will lose if you have more than 5 wrong guess.</span>
-          </div>
+        isOpen={openHelp}
+        onRequestClose={closeModal}
+        appElement={document.getElementById('root')}
+        className="a-modal"
+      >
+        <div className='help'>
+          <span>Try to get the secret word one letter at a time!</span>
+          <span>Keep in mind you will lose if you have more than 5 wrong guesses.</span>
+        </div>
       </Modal>
       <audio id="music">
         <source src={music} />
       </audio>
 
-      <Box sx={{ width: 200 }} className="box">
+      <Box className="box">
         <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
           {playing ? 
           <button 
@@ -111,7 +82,7 @@ const Control = ({volume, setVolume, musicVolume, setMusicVolume}) => {
         </Stack> 
       </Box>
 
-      <Box sx={{ width: 200 }} className="box">
+      <Box className="box">
         <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
           {volume ? 
           <button 
